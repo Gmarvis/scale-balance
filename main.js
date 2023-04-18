c51b91b29a378443fae1830f320280be251ce2ff
gmarvis <sgmarvis@gmail.com>
2023-04-18 11:16:09 +0100

    fist commit

---
+++ main.js
@@ -0,0 +1,103 @@
+let firstInput = document.getElementById('firstInput')
+let secondInput = document.getElementById('secondInput')
+// let inputFill = document.getElementById('inputFill')
+let balance = document.querySelector('button')
+
+const myArray = Array(20)
+.fill()
+.map(()=>Math.floor(20 * Math.random()))
+console.log(myArray)
+
+// let sum = myArray.reduce(function(a, b){
+//     return a + b
+// })
+
+
+balance.addEventListener( 'click' , function(){
+    // const newArr = JSON.parse(inputFill.value);
+
+    for (let i = 0;i <= myArray.length; i++){
+        for (let j = 0; j <= myArray.length; j++) {
+            if ((+firstInput.value + myArray[i])===(+secondInput.value + myArray[j])){
+                document.querySelector('p').innerHTML = `${myArray[i]} and ${myArray[j]}`
+            }
+            // return;
+
+            else if (+firstInput.value <=0 || +firstInput.value >20 || +secondInput.value <=0 || +secondInput.value >20) {
+                document.querySelector('p').innerHTML = `numbers should be within the range of 1 and 20`
+            }
+
+        }
+    }
+
+} )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// balance.addEventListener( 'click' , function(){
+//     const newArr = JSON.parse(inputFill.value);
+
+//     for (let i = 0;i <= newArr.length; i++){
+//         for (let j = 0; j <= newArr.length; j++) {
+//             if ((+firstInput.value + newArr[i])===(+secondInput.value + newArr[j])){
+//                 document.querySelector('p').innerHTML = `${newArr[i]} and ${newArr[j]}`
+//                 return;
+//             }
+//             else {
+
+//             }
+//         }
+//     }
+
+// } )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const myArray = Array(20)
+// .fill()
+// .map(()=>Math.floor(20 * Math.random()))
+// console.log(myArray)
+
+// let sum = myArray.reduce(function(a, b){
+//     return a + b
+// })
+// console.log(sum)
+
+
